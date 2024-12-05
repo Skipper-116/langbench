10a442def1d3b5fef0b5d5f5954a9895e7ffc7fe
Roy Chanunkha <roychanunkha@gmail.com>
2024-12-05 14:00:34 +0200

    chore: java sorting

--- javaCode/Main.java
+++ javaCode/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -12,6 +13,20 @@ public class Main {
         int[][] matrixA = generateMatrix(100, 100);
         int[][] matrixB = generateMatrix(100, 100);
         matrixMultiply(matrixA, matrixB);
+
+        int[] dataset = generateDataset(10000, 1000);
+
+        int[] quicksortArray = Arrays.copyOf(dataset, dataset.length);
+        quicksort(quicksortArray, 0, quicksortArray.length - 1);
+
+        int[] mergesortArray = Arrays.copyOf(dataset, dataset.length);
+        mergesort(mergesortArray, 0, mergesortArray.length - 1);
+
+        int[] heapsortArray = Arrays.copyOf(dataset, dataset.length);
+        heapsort(heapsortArray);
+
+        int[] insertionSortArray = Arrays.copyOf(dataset, dataset.length);
+        insertionSort(insertionSortArray);
 
         long endTime = System.nanoTime();
         double elapsedTime = (endTime - startTime) / 1e9;
@@ -32,25 +47,159 @@ public class Main {
         int[][] matrix = new int[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                matrix[i][j] = rand.nextInt(100);
+                matrix[i][j] = rand.nextInt(1000);
             }
         }
         return matrix;
     }
 
-    public static int[][] matrixMultiply(int[][] a, int[][] b) {
-        int rowsA = a.length;
-        int colsA = a[0].length;
-        int colsB = b[0].length;
+    public static void matrixMultiply(int[][] matrixA, int[][] matrixB) {
+        int rowsA = matrixA.length;
+        int colsA = matrixA[0].length;
+        int colsB = matrixB[0].length;
         int[][] result = new int[rowsA][colsB];
 
         for (int i = 0; i < rowsA; i++) {
             for (int j = 0; j < colsB; j++) {
                 for (int k = 0; k < colsA; k++) {
-                    result[i][j] += a[i][k] * b[k][j];
+                    result[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
             }
         }
-        return result;
+    }
+
+    public static int[] generateDataset(int size, int maxValue) {
+        Random rand = new Random();
+        int[] dataset = new int[size];
+        for (int i = 0; i < size; i++) {
+            dataset[i] = rand.nextInt(maxValue) + 1;
+        }
+        return dataset;
+    }
+
+    public static void quicksort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quicksort(array, low, pi - 1);
+            quicksort(array, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+
+    public static void mergesort(int[] array, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergesort(array, left, mid);
+            mergesort(array, mid + 1, right);
+            merge(array, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] array, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = array[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = array[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            array[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void heapsort(int[] array) {
+        int n = array.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(array, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+
+    public static void heapify(int[] array, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest]) {
+            largest = left;
+        }
+
+        if (right < n && array[right] > array[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            heapify(array, n, largest);
+        }
+    }
+
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+        for (int i = 1; i < n; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
     }
 }
