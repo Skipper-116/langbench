534176a3eb81667c0eba192ce6a629a8fd3c46b2
Roy Chanunkha <roychanunkha@gmail.com>
2024-12-05 14:14:54 +0200

    chore: c# sorts

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -14,6 +14,20 @@ class Program
         int[,] matrixB = GenerateMatrix(100, 100);
         MatrixMultiply(matrixA, matrixB);
 
+        // Generate dataset
+        int[] dataset = GenerateDataset(10000, 1000);
+
+        // Sort using different algorithms
+        int[] quicksortDataset = (int[])dataset.Clone();
+        int[] mergesortDataset = (int[])dataset.Clone();
+        int[] heapsortDataset = (int[])dataset.Clone();
+        int[] insertionSortDataset = (int[])dataset.Clone();
+
+        Quicksort(quicksortDataset, 0, quicksortDataset.Length - 1);
+        Mergesort(mergesortDataset, 0, mergesortDataset.Length - 1);
+        Heapsort(heapsortDataset);
+        InsertionSort(insertionSortDataset);
+
         stopwatch.Stop();
         Console.WriteLine($"Elapsed time C#: {stopwatch.Elapsed.TotalSeconds} seconds");
     }
@@ -60,5 +74,153 @@ class Program
             }
         }
         return result;
+    }
+
+    static int[] GenerateDataset(int size, int maxValue)
+    {
+        var random = new Random();
+        int[] dataset = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            dataset[i] = random.Next(1, maxValue + 1);
+        }
+        return dataset;
+    }
+
+    static void Quicksort(int[] array, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = Partition(array, low, high);
+            Quicksort(array, low, pi - 1);
+            Quicksort(array, pi + 1, high);
+        }
+    }
+
+    static int Partition(int[] array, int low, int high)
+    {
+        int pivot = array[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++)
+        {
+            if (array[j] < pivot)
+            {
+                i++;
+                Swap(array, i, j);
+            }
+        }
+        Swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    static void Mergesort(int[] array, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+            Mergesort(array, left, middle);
+            Mergesort(array, middle + 1, right);
+            Merge(array, left, middle, right);
+        }
+    }
+
+    static void Merge(int[] array, int left, int middle, int right)
+    {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        Array.Copy(array, left, leftArray, 0, n1);
+        Array.Copy(array, middle + 1, rightArray, 0, n2);
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2)
+        {
+            if (leftArray[i] <= rightArray[j])
+            {
+                array[k] = leftArray[i];
+                i++;
+            }
+            else
+            {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1)
+        {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2)
+        {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void Heapsort(int[] array)
+    {
+        int n = array.Length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            Heapify(array, n, i);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            Swap(array, 0, i);
+            Heapify(array, i, 0);
+        }
+    }
+
+    static void Heapify(int[] array, int n, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && array[left] > array[largest])
+            largest = left;
+
+        if (right < n && array[right] > array[largest])
+            largest = right;
+
+        if (largest != i)
+        {
+            Swap(array, i, largest);
+            Heapify(array, n, largest);
+        }
+    }
+
+    static void InsertionSort(int[] array)
+    {
+        int n = array.Length;
+        for (int i = 1; i < n; ++i)
+        {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
     }
 }
