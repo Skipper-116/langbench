af70bf8b0ef7a6495606b868c8ef4d799fba7352
Roy Chanunkha <roychanunkha@gmail.com>
2024-12-06 08:57:17 +0200

    chore: js search

--- jsCode/index.js
+++ jsCode/index.js
@@ -120,6 +120,33 @@ function insertionSort(arr) {
     return arr;
 }
 
+// Binary Search
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1; // Target not found
+}
+
+// Linear Search
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return -1; // Target not found
+}
+
 fibonacci(1000);
 
 let matrixA = generateMatrix(100, 100);
@@ -128,10 +155,16 @@ matrixMultiply(matrixA, matrixB);
 
 // Generate dataset
 let dataset = generateRandomDataset(10000, 1000);
-quicksort([...dataset]);
+let largeSortedArray = quicksort([...dataset]);
 mergesort([...dataset]);
 heapsort([...dataset]);
 insertionSort([...dataset]);
+
+let largeUnsortedArray = [...dataset];
+let target = Math.floor(Math.random() * 1000) + 1;
+
+binarySearch(largeSortedArray, target);
+linearSearch(largeUnsortedArray, target);
 
 const endTime = Date.now();
 const elapsedTime = (endTime - startTime) / 1000;
