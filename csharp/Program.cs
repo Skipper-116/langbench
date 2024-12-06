8995667765b0f8a1a0ef4e2dc0194de02dc7bdf7
Roy Chanunkha <roychanunkha@gmail.com>
2024-12-06 09:24:19 +0200

    chore: c# search

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -9,7 +9,10 @@ class Program
         var stopwatch = Stopwatch.StartNew();
         var memo = new Dictionary<int, long>();
         
+        // Fibonacci
         Fibonacci(1000, memo);
+
+        // Matrix multiplication
         int[,] matrixA = GenerateMatrix(100, 100);
         int[,] matrixB = GenerateMatrix(100, 100);
         MatrixMultiply(matrixA, matrixB);
@@ -23,13 +26,58 @@ class Program
         int[] heapsortDataset = (int[])dataset.Clone();
         int[] insertionSortDataset = (int[])dataset.Clone();
 
+        // Sort Algorithms
         Quicksort(quicksortDataset, 0, quicksortDataset.Length - 1);
         Mergesort(mergesortDataset, 0, mergesortDataset.Length - 1);
         Heapsort(heapsortDataset);
         InsertionSort(insertionSortDataset);
 
+        // Search Algorithms
+        int target = dataset[new Random().Next(0, dataset.Length)];
+        int[] sortedDataset = (int[])dataset.Clone();
+        Array.Sort(sortedDataset);
+        LinearSearch(dataset, target);
+        BinarySearch(sortedDataset, target);
+
         stopwatch.Stop();
         Console.WriteLine($"Elapsed time C#: {stopwatch.Elapsed.TotalSeconds} seconds");
+    }
+
+    static int LinearSearch(int[] array, int target)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == target)
+            {
+                return i;
+            }
+        }
+        return -1; // Not found
+    }
+
+    static int BinarySearch(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+            {
+                return mid;
+            }
+            if (array[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+        return -1; // Not found
     }
 
     static long Fibonacci(int n, Dictionary<int, long> memo)
